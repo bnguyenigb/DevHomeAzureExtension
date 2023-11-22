0e74cba62e5759c36af49fd78ea59288151e71cf
Huzaifa Danish <Huzaifa.Danish@microsoft.com>
2023-11-22 14:42:07 -0800

    Added Moq Test

--- test/AzureExtension/DevBox/DevBoxTests.cs
+++ test/AzureExtension/DevBox/DevBoxTests.cs
@@ -7,7 +7,6 @@ using AzureExtension.Services.DevBox;
 using AzureExtension.Test.DevBox;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Http;
 
 namespace DevHomeAzureExtension.Test;
 
@@ -15,8 +14,9 @@ public partial class DevBoxTests
 {
     [TestMethod]
     [TestCategory("Unit")]
-    public void DevBox_Parsing()
+    public void DevBox_MockHTTPCalls()
     {
+        // Arrange
         var host = Microsoft.Extensions.Hosting.Host.
             CreateDefaultBuilder().
             UseContentRoot(AppContext.BaseDirectory).
@@ -26,96 +26,21 @@ public partial class DevBoxTests
             }).
             ConfigureServices((context, services) =>
             {
-                services.AddSingleton<IDevBoxManagementService, MgmtTestService>();
-            }).
-            Build();
-
-        var instance = new DevBoxProvider(host);
-        var systems = instance.GetComputeSystemsAsync(null).Result;
-        Assert.IsTrue(systems.Any());
-    }
-
-    [TestMethod]
-    [TestCategory("Manual")]
-
-    // [Ignore("Comment out to run")]
-    public void DevBox_TokenAuth()
-    {
-        var host = Microsoft.Extensions.Hosting.Host.
-            CreateDefaultBuilder().
-            UseContentRoot(AppContext.BaseDirectory).
-            UseDefaultServiceProvider((context, options) =>
-            {
-                options.ValidateOnBuild = true;
-            }).
-            ConfigureServices((context, services) =>
-            {
-                services.AddHttpClient();
+                services.AddSingleton<IHttpClientFactory>(mockFactory.Object);
+                services.AddSingleton<HttpClient>(mockHttpClient);
                 services.AddSingleton<IDevBoxManagementService, ManagementService>();
                 services.AddSingleton<IDevBoxAuthService, AuthService>();
                 services.AddSingleton<IArmTokenService, ArmTestTokenService>();
                 services.AddSingleton<IDataTokenService, DataTestTokenService>();
+                services.AddTransient<DevBoxInstance>();
             }).
             Build();
 
+        // Act
         var instance = new DevBoxProvider(host);
         var systems = instance.GetComputeSystemsAsync(null).Result;
-        Assert.IsTrue(systems.Any());
-    }
 
-    [TestMethod]
-    [TestCategory("Manual")]
-
-    // [Ignore("Comment out to run")]
-    public void DevBox_DataTokenAuth()
-    {
-        var host = Microsoft.Extensions.Hosting.Host.
-            CreateDefaultBuilder().
-            UseContentRoot(AppContext.BaseDirectory).
-            UseDefaultServiceProvider((context, options) =>
-            {
-                options.ValidateOnBuild = true;
-            }).
-            ConfigureServices((context, services) =>
-            {
-                services.AddHttpClient();
-                services.AddSingleton<IDevBoxManagementService, ManagementService>();
-                services.AddSingleton<IDevBoxAuthService, AuthService>();
-                services.AddSingleton<IArmTokenService, ArmTestTokenService>();
-                services.AddSingleton<IDataTokenService, DataTokenService>();
-            }).
-            Build();
-
-        var instance = new DevBoxProvider(host);
-        var systems = instance.GetComputeSystemsAsync(null).Result;
-        Assert.IsTrue(systems.Any());
-    }
-
-    [TestMethod]
-    [TestCategory("Manual")]
-
-    // [Ignore("Comment out to run")]
-    public void DevBox_Auth()
-    {
-        var host = Microsoft.Extensions.Hosting.Host.
-            CreateDefaultBuilder().
-            UseContentRoot(AppContext.BaseDirectory).
-            UseDefaultServiceProvider((context, options) =>
-            {
-                options.ValidateOnBuild = true;
-            }).
-            ConfigureServices((context, services) =>
-            {
-                services.AddHttpClient();
-                services.AddSingleton<IDevBoxManagementService, ManagementService>();
-                services.AddSingleton<IDevBoxAuthService, AuthService>();
-                services.AddSingleton<IArmTokenService, ArmTestTokenService>();
-                services.AddSingleton<IDataTokenService, DataTokenService>();
-            }).
-            Build();
-
-        var instance = new DevBoxProvider(host);
-        var systems = instance.GetComputeSystemsAsync(null).Result;
+        // Assert
         Assert.IsTrue(systems.Any());
     }
 }
